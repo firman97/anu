258df2cf74804d63185882917c5b5a722e424da0
firman97 <firmansidqi40@gmail.com>
2020-01-13 14:35:53 +0700

    mengisi halaman home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('web/home');
+		$this->load->view('front/layout/wrapper');
 	}
 }
